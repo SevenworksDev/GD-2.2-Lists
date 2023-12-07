b5124cf30c4cb230d2654c6e329ecc8338daff83
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2023-12-07 07:37:01 -0600

    Create getGJLevelLists.php

---
+++ database/getGJLevelLists.php
@@ -0,0 +1,31 @@
+<?php
+# havent tested much yet
+$host = "localhost";
+$username = "root";
+$password = "Tw0_Truck$";
+$database = "lists";
+$mysqli = new mysqli($host, $username, $password, $database);
+if ($mysqli->connect_error) { die("Connection failed: " . $mysqli->connect_error); }
+
+$query = "SELECT * FROM lists";
+$result = $mysqli->query($query);
+
+if ($result->num_rows > 0) {
+    $response = '';
+    $count = 0;
+    while ($row = $result->fetch_assoc()) {
+        $dateTimeUploaded = new DateTime($row['uploaded_timestamp']);
+        $dateTimeUpdated = new DateTime($row['updated_timestamp']);
+        $response .= "1:{$row['id']}:2:{$row['name']}:3:" . base64_encode($row['description']) . ":5:{$row['version']}:49:{$row['accountid']}:50:{$row['username']}:10:{$row['downloads']}:7:{$row['difficulty']}:14:{$row['likes']}:19:{$row['featuredIdx']}:51:{$row['levels']}:55:0:56:0:28:{$dateTimeUploaded->getTimestamp()}:29:{$dateTimeUpdated->getTimestamp()}|";
+        $count++;
+
+        if ($count >= 10) { break; }
+    }
+    if ($count > 0) { $response = rtrim($response, "|"); }
+    echo $response . "#16:RobTop:71#1:0:{$count}";
+} else {
+    echo "-1";
+}
+
+$mysqli->close();
+?>
